dadc746bd370e5b705f17c85fdd1d1b00aba8ddf
Oona Carbonie <carbonie.oona@outlook.com>
2019-09-28 17:01:08 -0400

    First push

--- src/main/java/com/medhacks/walkingangels/oona/Education.java
+++ src/main/java/com/medhacks/walkingangels/oona/Education.java
@@ -5,5 +5,36 @@ import com.medhacks.walkingangels.jen.Service;
 import javax.persistence.Entity;
 
 @Entity
-public class Education extends Service {
+public class Education extends Service{
+    private String teach;
+    private String learn;
+    private String other;
+
+    public Education() {
+    }
+
+    public String getTeach() {
+        return teach;
+    }
+
+    public void setTeach(String teach) {
+        this.teach = teach;
+    }
+
+    public String getLearn() {
+        return learn;
+    }
+
+    public void setLearn(String learn) {
+        this.learn = learn;
+    }
+
+    public String getOther() {
+        return other;
+    }
+
+    public void setOther(String other) {
+        this.other = other;
+    }
 }
+
