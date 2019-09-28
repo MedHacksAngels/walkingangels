c791072a759c8058cd8cdd83cc068a1e220907bd
jcblefler <jcb.lefler@gmail.com>
2019-09-28 14:26:07 -0400

    initial commit

---
+++ src/test/java/com/medhacks/walkingangels/WalkingAngelsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.medhacks.walkingangels;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WalkingAngelsApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
